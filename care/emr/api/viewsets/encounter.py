3da44895883c57329bbc0c9ad89e0d5e6bae50e8
vigneshhari <vichuhari100@gmail.com>
2025-04-03 11:08:14 +0530

    Deny same user repeating in the same care team.

--- care/emr/api/viewsets/encounter.py
+++ care/emr/api/viewsets/encounter.py
@@ -292,10 +292,12 @@ class EncounterViewSet(
         self.authorize_update({}, encounter)
 
         members = []
-
+        users = []
         for member in request_data.members:
             user_obj = get_object_or_404(User, external_id=member.user_id)
-
+            if user_obj.id in users:
+                raise ValidationError({"user" : "repeats are not allowed"})
+            users.append(user_obj.id)
             if not AuthorizationController.call(
                 "can_view_encounter_obj", request.user, encounter
             ):
