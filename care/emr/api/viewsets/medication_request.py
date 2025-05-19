e91c8cdb7a25867fd2981f901bdd1fb59a6d9332
vigneshhari <vichuhari100@gmail.com>
2025-05-19 17:53:01 +0530

    Add dispense status filter

--- care/emr/api/viewsets/medication_request.py
+++ care/emr/api/viewsets/medication_request.py
@@ -91,6 +91,7 @@ class MedicationRequestSummaryFilters(filters.FilterSet):
     category = filters.CharFilter(lookup_expr="iexact")
     patient_external_id = filters.UUIDFilter(field_name="patient__external_id")
     encounter_external_id = filters.UUIDFilter(field_name="encounter__external_id")
+    dispense_status = filters.CharFilter(lookup_expr="iexact")
 
 
 class MedicationRequestSummaryViewSet(EMRBaseViewSet):
