3e39443d4efcfa3adf0c28d325edd54d0bc185d4
vigneshhari <vichuhari100@gmail.com>
2025-07-09 14:11:12 +0530

    Fix issue with locks

--- care/emr/locks/billing.py
+++ care/emr/locks/billing.py
@@ -6,8 +6,10 @@ from care.utils.lock import Lock
 class AccountLock(Lock):
     def __init__(self, account, timeout=settings.LOCK_TIMEOUT):
         self.key = f"lock:account:{account.id}"
+        self.timeout = timeout
 
 
 class InvoiceLock(Lock):
     def __init__(self, invoice, timeout=settings.LOCK_TIMEOUT):
         self.key = f"lock:invoice:{invoice.id}"
+        self.timeout = timeout
